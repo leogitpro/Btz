5ce00a552e9369d43f9fed7395bfc974651e59c4
Leo <leoemailbox@163.com>
2016-12-24 22:27:56 +0800

    done log rebuilded

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -18,43 +18,12 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        /**
-        echo '<pre>';
-        print_r($this->config()->get('log'));
-        print_r($this->config()->get('service_manager'));
-        echo '</pre>';
-        //*/
-
-        $this->logger()->debug('Controller Logger Plugin test');
-
-        /**
-        $sm = $this->getEvent()->getApplication()->getServiceManager();
-        $logger = $sm->get("Logger");
-        $logger->debug('This is a test debug at: ' . date('c'));
-        $logger->info('iTest info log');
-        $logger->emerg('Emerg message test');
-        //*/
-        //$logger->emerg("emerg message test");
-
-        //$config = $sm->get("config");
-        //echo '<pre>'; print_r($config); echo '</pre>';
-        //var_dump($this->host()->getHost());
-
-        //$arr = $this->config()->get('mail.smtp.connection_config');
-        //echo '<pre>'; print_r($arr); echo '</pre>';
 
         return new ViewModel();
     }
 
     public function testAction()
     {
-        $captcha = Factory::factory($this->config()->get('captcha'));
-        $id = $captcha->generate();
-
-        var_dump($captcha->getWord());
-
-        $imgUrl = $this->getRequest()->getBaseUrl() . $captcha->getImgUrl() . $captcha->getId() . $captcha->getSuffix();
-
         return new ViewModel(['img' => $imgUrl]);
     }
 }
